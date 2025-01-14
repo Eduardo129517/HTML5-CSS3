79bcbcd84f1772888b8347300c7c98ce7979909e
Eduardo129517 <eduardoluis1295@gmail.com>
2025-01-14 20:25:23 -0300

    Add files via upload

---
+++ JavaScript/Aulas/Aula_4/DeclarandoVariaveis.js
@@ -0,0 +1,8 @@
+var NUMERO = 10;
+console.log(NUMERO);
+
+let novoNumero = 15;
+console.log(novoNumero);
+
+const OLAMUNDO = "Olá mundo";
+console.log(OLAMUNDO);
---
+++ JavaScript/Aulas/Aula_6/arraysEObject.js
@@ -0,0 +1,20 @@
+vetorVazio = [];
+vogais = ["A", "E", "I", "O", "U"];
+console.log(vogais);
+primeiraVogal = vogais[0];
+console.log(primeiraVogal);
+
+objetoVazio = {};
+cliente = {
+    nome : "antônio",
+     idade : 32
+    };
+console.log(cliente);
+
+nomeCliente = cliente.nome;
+console.log(nomeCliente);
+console.log(cliente.nome);
+
+idadeCliente = cliente["idade"];
+console.log(idadeCliente);
+console.log(cliente["idade"]);
