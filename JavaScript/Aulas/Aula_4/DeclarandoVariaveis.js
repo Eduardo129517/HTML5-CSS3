8f53da43da94816a62dfed6ec707912061d16144
Eduardo129517 <eduardoluis1295@gmail.com>
2025-02-04 20:44:17 -0300

    Delete JavaScript directory

--- JavaScript/Aulas/Aula_4/DeclarandoVariaveis.js
+++
@@ -1,8 +0,0 @@
-var NUMERO = 10;
-console.log(NUMERO);
-
-let novoNumero = 15;
-console.log(novoNumero);
-
-const OLAMUNDO = "Olá mundo";
-console.log(OLAMUNDO);
--- JavaScript/Testes/calculadora.js
+++
@@ -1,5 +0,0 @@
-function soma (numero1 , numero2){
-    let resultado = numero1 + numero2;
-    console.log(resultado);
-}
-soma(3, 2);
